9ad9cbadd5cd6d58683d5331da804ff84f325218
KD-huhu <64901690+KD-huhu@users.noreply.github.com>
2020-07-18 13:28:56 +0800

    Add files via upload

---
+++ _35_Shallow_deep_copy/main.cpp
@@ -0,0 +1,57 @@
+#include <iostream>
+using namespace std;
+
+class Car {
+	int m_price;
+	char *m_name;
+	void copy(const char *name = NULL) {
+		if (name == NULL) return;
+		// 申请新的堆空间
+		m_name = new char[strlen(name) + 1] {};
+		// 拷贝字符串数据到新的堆空间
+		strcpy(m_name, name);
+	}
+public:
+	Car(int price = 0, const char *name = NULL) :m_price(price) {
+		copy(name);
+	}
+	Car(const Car &car) :m_price(car.m_price) {
+		copy(car.m_name);
+	}
+	~Car() {
+		if (m_name == NULL) return;
+
+		delete[] m_name;
+		m_name = NULL;
+	}
+	void display() {
+		cout << "price is " << m_price << ", name is " << m_name << endl;
+	}
+};
+
+//class Car {
+//	int m_price;
+//	char* m_name;
+//};
+//
+//class Person {
+//	int m_age;
+//	Car m_car;
+//};
+
+int main() {
+	//Person person1;
+	//Person person2 = person1;
+
+	/*Car car1(100, "bmw");
+	Car car2 = car1;
+	car2.display();*/
+
+	Car *car = new Car(100, "bmw");
+	car->display();
+
+	// delete[] name;
+
+	getchar();
+	return 0;
+}
---
+++ _38_Compiler_generated_constructor/main.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+using namespace std;
+
+//class Person {
+//public:
+//	int m_age = 0;
+//};
+
+//class Student : public Person {
+//public:
+//
+//};
+
+//class Car {
+//public:
+//	int m_price = 0;
+//};
+
+//class Person {
+//public:
+//	Car car;
+//};
+
+class Student {
+public:
+	int m_price;
+	void run() {
+
+	}
+};
+
+int main() {
+	Student student;
+	student.run();
+
+	getchar();
+	return 0;
+}
